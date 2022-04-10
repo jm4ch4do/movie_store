683abeab54d33ebc49ac214468a071ecb1dfb957
José Raúl Machado Fernández <m4ch4do@protonmail.com>
2022-04-10 14:50:19 -0400

    Add Book model and other minor fixes

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(blank=False, unique=True, max_length=200)
+    description = models.TextField(max_length=256, blank=True)
+    price = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    last_modified = models.DateField(auto_now=True)
+    published = models.DateField(blank=True, null=True, default=None)
+    cover = models.ImageField(upload_to='covers/', blank=True)
+
+    def __str__(self):
+        return self.title
