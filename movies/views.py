123ce7ac99e9e616fe487097941d301f2347e204
José Raúl Machado Fernández <m4ch4do@protonmail.com>
2022-04-12 13:12:01 -0400

    Improved token access config

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.authentication import TokenAuthentication
 
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    authentication_classes = (TokenAuthentication,)
