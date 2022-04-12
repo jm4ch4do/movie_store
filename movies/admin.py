c524537b2723bb4817b133bd1be26ff504512b0b
José Raúl Machado Fernández <m4ch4do@protonmail.com>
2022-04-12 09:12:53 -0400

    Customized admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+# admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_editable = ['description']
+    list_filter = ['is_published']
+    search_fields = ['title']
+
+
